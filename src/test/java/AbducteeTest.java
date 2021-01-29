3653e167f527ba1334c5259f556107449f491691
Robyn Tyler <robynstyler@outlook.com>
2021-01-29 22:42:45 +0000

    Created first Abductee Test.

---
+++ src/test/java/AbducteeTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AbducteeTest {
+
+    private Abductee abductee;
+
+    @Before
+    public void before(){
+        abductee = new Abductee("Human", 10);
+    }
+
+    @Test
+    public void abducteeHasSpecies(){
+        assertEquals("Human", abductee.getSpecies());
+    }
+
+}
