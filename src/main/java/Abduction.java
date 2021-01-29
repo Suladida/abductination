ecf639b8c11115717f35838dc8c843dbc83adbfe
Robyn Tyler <robynstyler@outlook.com>
2021-01-29 23:12:49 +0000

    Abduction class created.

---
+++ src/main/java/Abduction.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Abduction {
+
+    ArrayList<Abductee> abductees;
+    Abductee abductee;
+    Spacecraft spacecraft;
+
+    public ArrayList<Abductee> getAbductees(){
+        return abductees;
+    }
+
+
+}
+
